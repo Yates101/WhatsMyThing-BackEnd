900d9bd7b6476c0ff4f9f603741286cec1b2b8b5
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2021-07-13 13:23:06 +0200

    router updated

--- router.js
+++ router.js
@@ -1,4 +1,5 @@
 let router = require("express").Router();
+let thingController = require("./thingController");
 
 router.get("/", (req, res) => {
 	res.json({
@@ -6,5 +7,14 @@ router.get("/", (req, res) => {
 		message: "Test Test test Welcome to WhatsMyThing API",
 	});
 });
+
+router.route("/thing").get(thingController.index).get(thingController.add);
+
+router
+	.route("/thing/:thing_id")
+	.get(thingController.view)
+	.patch(thingController.update)
+	.put(thingController.update)
+	.delete(thingController.delete);
 
 module.exports = router;
